049e06813622bfd27055d6a78b7efb709085c8be
SimonPhilipp <simon.philipp@e.mail.de>
2018-02-23 21:55:42 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,41 @@
-function explorer(address,tab) {
-  chrome.tabs.create({  
-    url: "https://explorer.lisk.io/address/" + address.selectionText,
-  });           
-}
-function sendWithNano(address,tab) {
-  chrome.tabs.create({  
-    url: "lisk://main/transaction/send?recipient=" + address.selectionText,
-  });           
+function isAddress(address) {
+	result = false;
+	if(/d*L/.test(address)){
+		result = true;
+	}
+	else{
+		var notifOptions = {
+                        type: "basic",
+                        iconUrl: "favicon.png",
+                        title: "Not a Lisk address!",
+                        message: "Uh oh, that's not a valid Lisk address."
+                    };
+                    chrome.notifications.create('limitNotif', notifOptions);
+	}
+	return result;
 }
 
-function voteManager(address,tab) {
+function explorer(address,tab) {
+		if(isAddress(address.selectionText)){
+		chrome.tabs.create({  
+			url: "https://explorer.lisk.io/address/" + address.selectionText,
+		});
+		}
+}
+
+function sendWithNano(address,tab) {
+	if(isAddress(address.selectionText)){
   chrome.tabs.create({  
-    url: "https://lisk.builders/votemanager/" + address.selectionText,
-  });           
+    url: "lisk://main/transactions/send?recipient=" + address.selectionText,
+  });   
+	}  
+}
+function voteManager(address,tab) {
+		if(isAddress(address.selectionText)){
+		chrome.tabs.create({  
+			url: "https://lisk.builders/votemanager/" + address.selectionText,
+		});
+		}
 }
 
 chrome.contextMenus.create({
