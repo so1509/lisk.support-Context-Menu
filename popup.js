a17f0d68ed18d723b9dcc921dba422f176ca379b
Sebastian <Sebastian@WorkPC.localdomain>
2018-03-05 20:20:01 +0100

    Added: options.css, options.html, popup.cssChanged popup.js / Removed style.css

--- popup.js
+++ popup.js
@@ -1,6 +1,41 @@
+window.onload = function() {
+	document.getElementById('donate').addEventListener('click', donate);
+	document.getElementById('options').addEventListener('click', options);
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+	var xhr = new XMLHttpRequest();
+	var url = "https://api.coinmarketcap.com/v1/ticker/lisk/?convert=EUR";
+	xhr.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			var json = JSON.parse(this.responseText);
+			myFunction(json);
+		}
+	};
+	xhr.open("GET", url, true);
+	xhr.send();
+
+	function myFunction(arr) {
+		var lskToCur = round(arr[0].price_eur, 4);
+		var lskToBtc = round(arr[0].price_btc, 6);
+
+		// Number(Math.round(1.005+'e2')+'e-2'); // 1.01
+		document.getElementById('currency').innerHTML = lskToCur;
+		document.getElementById('btc').innerHTML = lskToBtc;
+	}
+});
+
+function round(value, decimals) {
+	return Number(Math.round(value + 'e' + decimals) + 'e-' + decimals);
+}
 function donate(tab) {
-		chrome.tabs.create({  
-			url: "lisk://main/transactions/send?recipient=5471811853702363574L",
-		});   
-	  
+	chrome.tabs.create({
+		url : "lisk://main/transactions/send?recipient=5471811853702363574L",
+	});
+}
+function options(tab) {
+	chrome.tabs
+			.create({
+				url : "chrome-extension://nfgaafodnbacmnkjihomkkolbihcnohg/options.html",
+			});
 }
