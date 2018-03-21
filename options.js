02eeebd470c91a8d7f77e64d5ba087d0ddc68975
Sebastian <Sebastian@WorkPC.localdomain>
2018-03-21 18:39:24 +0100

    finished design for popup and options page

--- options.js
+++ options.js
@@ -1,10 +1,13 @@
+window.onload = function() {
+	document.getElementById('save').addEventListener('click', save_options);
+}
 // Saves options to chrome.storage
 function save_options() {
-	var color = document.getElementById('color').value;
-	var likesColor = document.getElementById('like').checked;
+	var currencyToSave = document.getElementById('currency').value;
+	var storedLSKAdd = document.getElementById('storedLSKAdd').value;
 	chrome.storage.sync.set({
-		favoriteColor : color,
-		likesColor : likesColor
+		currency : currencyToSave,
+		storedLSKAdd : storedLSKAdd
 	}, function() {
 		// Update status to let user know options were saved.
 		var status = document.getElementById('status');
@@ -18,14 +21,15 @@ function save_options() {
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
 function restore_options() {
-	// Use default value color = 'red' and likesColor = true.
+	// Use default value currency = 'USD' if none specified yet
 	chrome.storage.sync.get({
-		favoriteColor : 'red',
-		likesColor : true
-	}, function(items) {
-		document.getElementById('color').value = items.favoriteColor;
-		document.getElementById('like').checked = items.likesColor;
+		currency : 'usd', storedLSKAdd:'Your LSK Adress'
+	}, function(result) {
+		var storedCurrency = result.currency.toUpperCase();
+		document.getElementById('currency').value = storedCurrency;
+		
+		var storedLSKAdd = result.storedLSKAdd;
+		document.getElementById('storedLSKAdd').value = storedLSKAdd;
 	});
 }
 document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('save').addEventListener('click', save_options);
--- popup.js
+++ popup.js
@@ -1,28 +1,38 @@
 window.onload = function() {
 	document.getElementById('donate').addEventListener('click', donate);
 	document.getElementById('options').addEventListener('click', options);
+	document.getElementById('lskImage').addEventListener('click', explorer);
 }
+document.addEventListener('DOMContentLoaded', function() {
+	chrome.storage.sync.get({
+		currency : 'usd'
+	}, function(result) {
+		var currencyConv = result.currency;
+		var xhr = new XMLHttpRequest();
+		var url = 'https://api.coinmarketcap.com/v1/ticker/lisk/?convert='
+				+ currencyConv;
+		xhr.onreadystatechange = function() {
+			if (this.readyState == 4 && this.status == 200) {
+				var json = JSON.parse(this.responseText);
+				jsonConvert(json);
+			}
+		};
+		xhr.open("GET", url, true);
+		xhr.send();
+		function jsonConvert(json) {
+			var lskToCur = round(json[0]['price_' + currencyConv.toLowerCase()], 4);
+			var currencyCode = currencyConv.toUpperCase();
+			var lskToBtc = round(json[0].price_btc, 6);
 
-document.addEventListener("DOMContentLoaded", function() {
-	var xhr = new XMLHttpRequest();
-	var url = "https://api.coinmarketcap.com/v1/ticker/lisk/?convert=EUR";
-	xhr.onreadystatechange = function() {
-		if (this.readyState == 4 && this.status == 200) {
-			var json = JSON.parse(this.responseText);
-			myFunction(json);
+			// Number(Math.round(1.005+'e2')+'e-2');
+			// // 1.01
+			document.getElementById('currencyValue').innerHTML = lskToCur;
+			document.getElementById('btcValue').innerHTML = lskToBtc;
+
+			document.getElementById('currencyCode').innerHTML = currencyCode;
+			document.getElementById('currencyCode1').innerHTML = currencyCode;
 		}
-	};
-	xhr.open("GET", url, true);
-	xhr.send();
-
-	function myFunction(arr) {
-		var lskToCur = round(arr[0].price_eur, 4);
-		var lskToBtc = round(arr[0].price_btc, 6);
-
-		// Number(Math.round(1.005+'e2')+'e-2'); // 1.01
-		document.getElementById('currency').innerHTML = lskToCur;
-		document.getElementById('btc').innerHTML = lskToBtc;
-	}
+	});
 });
 
 function round(value, decimals) {
@@ -38,4 +48,9 @@ function options(tab) {
 			.create({
 				url : "chrome-extension://nfgaafodnbacmnkjihomkkolbihcnohg/options.html",
 			});
+}
+function explorer(tab) {
+	chrome.tabs.create({
+		url : "https://explorer.lisk.io/",
+	});
 }
